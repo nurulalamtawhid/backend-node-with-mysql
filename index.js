1a4a8853f196555b8b2a4b0cd145e44209bc03e4
unknown <tawhidalam69@gmail.com>
2024-05-29 10:57:07 +0600

    delete api

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-import express from "express"
+import express from "express"//in the package.json file just "type" :"module" for got this import
 
 import mysql2 from "mysql2"
 
+import cors from "cors"
+
+
+
+
+
 
 const app = express();
-app.use(express.json());
-
+app.use(express.json());//use this in express server for working with json file
+app.use(cors());
 const db = mysql2.createConnection({
     host: "localhost",
-    user:"root",
+    user:"root",                       // DATABASE connection with my sql 
     password:"tnt69ddt4",
-    database:"test" 
+    database:"test"                 //same as db file name
 })
 
 app.get("/",(req,res)=>{
@@ -26,15 +32,26 @@ app.get("/books",(req,res)=>{
     })
 })
 app.post("/books",(req,res)=>{
-    const q = "INSERT INTO books (`tittle`,`desc`,`cover`) VALUES (?)"
+    const q = "INSERT INTO books (`tittle`,`desc`,`cover`,`price`) VALUES (?)"
     const values = [
         req.body.tittle,
         req.body.desc,
         req.body.cover,
+        req.body.price,
     ]
     db.query(q,[values],(err,data)=>{
       if (err) return res.json(err);
       return res.json("created successfully");  
+    })
+})
+app.delete("/books/:id",(req,res)=>{
+    const bookID = req.params.id;
+    const q = "DELETE from books WHERE id = ?";
+
+
+    db.query(q,[bookID],(err,data)=>{
+        if(err) return res.json(err);
+        return res.json("Deleted Successfully");
     })
 })
 
